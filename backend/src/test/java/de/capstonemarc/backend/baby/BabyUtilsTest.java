9ac24160270b0adb3ce8fa360a12a342a6583365
Marc120985 <creativ.zeit@gmail.com>
2022-11-21 14:17:17 +0100

    add test generateUUID

---
+++ backend/src/test/java/de/capstonemarc/backend/baby/BabyUtilsTest.java
@@ -0,0 +1,18 @@
+package de.capstonemarc.backend.baby;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BabyUtilsTest {
+
+    @Test
+    void generateUUID() {
+        //given
+        BabyUtils babyUtils = new BabyUtils();
+        //when
+        String actual = babyUtils.generateUUID();
+        //then
+        assertNotNull(actual);
+    }
+}
