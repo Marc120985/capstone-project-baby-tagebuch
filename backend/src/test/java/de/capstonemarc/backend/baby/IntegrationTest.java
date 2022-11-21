f5752df43e62a832e84f1f3bcc13cd4a76c35804
Marc120985 <creativ.zeit@gmail.com>
2022-11-21 17:34:15 +0100

    add junit and IntegrationTest

--- backend/src/test/java/de/capstonemarc/backend/baby/IntegrationTest.java
+++ backend/src/test/java/de/capstonemarc/backend/baby/IntegrationTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -20,6 +22,7 @@ class IntegrationTest {
     private ObjectMapper objectMapper;
 
     @Test
+    @DirtiesContext
     void addBaby() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/baby")
                 .contentType(APPLICATION_JSON)
@@ -28,5 +31,13 @@ class IntegrationTest {
                 .andExpect(status().is(201));
 
 
+    }
+
+    @Test
+    @DirtiesContext
+    void getAllBabies() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/baby"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 }
