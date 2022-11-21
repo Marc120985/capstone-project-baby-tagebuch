c9529f0290b4b767fdefbaf40034bcad908437d6
Marc120985 <creativ.zeit@gmail.com>
2022-11-21 16:45:12 +0100

    fix frontend Post

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
 import Babyoverview from "./Babyoverview";
+import styled from "styled-components";
 
 
 export default function App() {
-  return (
-    <main >
+  return <>
+      <SyledHeader>
+        <h1>Willkommen bei deinem Baby Tagebuch</h1>
+      </SyledHeader>
+
+      <main>
     <Routes>
       <Route path="/babyoverview" element={<Babyoverview/>}/>
-
-
     </Routes>
 
     </main>
-  );
+
+    <footer></footer>
+  </>;
 }
 
+
+const SyledHeader = styled.header`
+display: flex;
+justify-content: center;
+h1 {font-family: ubuntu,sans-serif;}
+`
