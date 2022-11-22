848e7b7419bcfd55a39839a7042982c32bbcd886
Marc120985 <creativ.zeit@gmail.com>
2022-11-22 12:16:19 +0100

    add page new Baby

--- frontend/src/Babyoverview.tsx
+++ frontend/src/Babyoverview.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 export default function Babyoverview() {
     const [babyname, setBabyname] = useState("")
@@ -30,7 +31,7 @@ export default function Babyoverview() {
         return <>
             <h1>Baby Overview</h1>
             <section>
-                <ul>{babies.map((baby: any) => <li key={baby.name}>{baby.name}</li>)}</ul>
+                <ul>{babies.map((baby: any) => <li key={baby.id}>{baby.name}</li>)}</ul>
                 <form onSubmit={postNewBaby}>
                     <label htmlFor="babyname">Name deines Baby's</label>
                     <input id="babyname" onChange={(event: ChangeEvent<HTMLInputElement>) =>
@@ -39,6 +40,8 @@ export default function Babyoverview() {
                     <button type="submit">Senden</button>
                 </form>
             </section>
+            <Link to={"/newbaby"}>Neues Baby anlegen</Link><br/>
+            <Link to={"/"}>Zurück zur Übersicht</Link><br/>
             </>;
     }
 
