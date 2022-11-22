ce2d128755260ddc139adb7dc88985d0e6179d09
Marc120985 <creativ.zeit@gmail.com>
2022-11-22 12:37:24 +0100

    remove newBaby from Babyoverview, add link to /baby

--- backend/src/main/java/de/capstonemarc/backend/baby/Baby.java
+++ backend/src/main/java/de/capstonemarc/backend/baby/Baby.java
@@ -2,6 +2,10 @@ package de.capstonemarc.backend.baby;
 
 public record Baby(
         String id,
-        String name
+        String name,
+        String birthday,
+        String weight,
+        String height,
+        String gender
 ) {
 }
--- backend/src/main/java/de/capstonemarc/backend/baby/NewBaby.java
+++ backend/src/main/java/de/capstonemarc/backend/baby/NewBaby.java
@@ -1,6 +1,10 @@
 package de.capstonemarc.backend.baby;
 
 public record NewBaby(
-        String name
+        String name,
+        String birthday,
+        String weight,
+        String height,
+        String gender
 ) {
 }
