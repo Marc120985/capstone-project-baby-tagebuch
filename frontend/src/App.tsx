55fb463824d8be64d2a4f6204102815f06b88a30
Marc120985 <creativ.zeit@gmail.com>
2022-11-23 08:29:28 +0100

    add Homepage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React, {useEffect} from 'react';
 import {Route, Routes} from "react-router";
 import Babyoverview from "./Babyoverview";
-import styled from "styled-components";
 import NewBaby from "./NewBaby";
-import { Link } from 'react-router-dom';
 import BabyPage from "./BabyPage";
 import axios from "axios";
+import Homepage from "./Homepage";
 
 
 export default function App() {
@@ -15,34 +14,31 @@ export default function App() {
 
     const getAllBabies = () => {
         axios.get("/api/baby")
-            .then((response) => {return response.data})
-            .then((data) => {setBabies(data)})
-            .catch((error) => {console.error("No Babies found! : " + error)})
+            .then((response) => {
+                return response.data
+            })
+            .then((data) => {
+                setBabies(data)
+            })
+            .catch((error) => {
+                console.error("No Babies found! : " + error)
+            })
     }
     useEffect(getAllBabies, [])
 
 
-  return <>
-      <SyledHeader>
-        <h1>Willkommen bei deinem Baby Tagebuch</h1>
-      </SyledHeader>
+    return <>
+        <main>
+            <Routes>
+                <Route path={"/"} element={<Homepage/>}/>
+                <Route path={"/babyoverview"} element={<Babyoverview babies={babies}/>}/>
+                <Route path={"/newbaby"} element={<NewBaby getAllBabies={getAllBabies}/>}/>
+                <Route path={"/baby/:id"} element={<BabyPage babies={babies}/>}/>
+            </Routes>
+        </main>
 
-      <main>
-    <Routes>
-      <Route path="/babyoverview" element={<Babyoverview babies={babies}/>}/>
-        <Route path={"/newbaby"} element={<NewBaby getAllBabies={getAllBabies}/>}/>
-        <Route path={"/baby/:id"} element={<BabyPage babies={babies}/>}/>
-    </Routes>
-    <Link to={"/babyoverview"}>Auswahl Baby</Link>
-    </main>
-
-    <footer></footer>
-  </>;
+    </>;
 }
 
 
-const SyledHeader = styled.header`
-display: flex;
-justify-content: center;
-h1 {font-family: ubuntu,sans-serif;}
-`
+
