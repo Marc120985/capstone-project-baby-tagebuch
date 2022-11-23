097ff112f286fbddfb45b7f8420c8e94aa0eb8cd
Marc120985 <creativ.zeit@gmail.com>
2022-11-23 10:25:41 +0100

    add img in Homepage.tsx

--- frontend/src/Homepage.tsx
+++ frontend/src/Homepage.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 
+
 export default function Homepage() {
     return <>
-        <SyledHeader>
+        <StyledHeader>
             <h1>Willkommen bei deinem Baby Tagebuch</h1>
-        </SyledHeader>
+        </StyledHeader>
         <main>
+            <StyledDiv>
+                <StyledImg src={process.env.PUBLIC_URL + '/BabyDiary_k.png'} alt="Baby"/>
+            </StyledDiv>
             <Link to={"/babyoverview"}>Auswahl Baby</Link>
 
         </main>
@@ -16,7 +20,8 @@ export default function Homepage() {
 }
 
 
-const SyledHeader = styled.header`
+const StyledHeader = styled.header`
+  background-color: var(--color-white);
   display: flex;
   justify-content: center;
 
@@ -25,3 +30,14 @@ const SyledHeader = styled.header`
   }
 `
 
+const StyledImg = styled.img`
+  width: 80%;
+  height: 80%;
+  justify-content: center;
+`
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: white;
+`
