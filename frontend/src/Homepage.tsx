afb71b31d14f1da1f02b839b4b3a5974e62962e1
Marc120985 <creativ.zeit@gmail.com>
2022-11-23 16:32:00 +0100

    fixed issues

--- frontend/src/Homepage.tsx
+++ frontend/src/Homepage.tsx
@@ -8,14 +8,10 @@ export default function Homepage() {
         <StyledHeader>
             <h1>Dein Baby Tagebuch</h1>
         </StyledHeader>
-        <main>
-            <StyledDiv>
-                <StyledImg src={process.env.PUBLIC_URL + '/BabyDiary_k.png'} alt="Baby"/>
-            </StyledDiv>
-            <StyledDiv2>
-                <StyledButton><StyledLink to={"/babyoverview"}>Eingang</StyledLink></StyledButton>
-            </StyledDiv2>
-        </main>
+        <StyledMain>
+            <StyledImg src={process.env.PUBLIC_URL + '/BabyDiary_k.png'} alt="Baby"/>
+            <StyledButton><StyledLink to={"/babyoverview"}>Eingang</StyledLink></StyledButton>
+        </StyledMain>
         <footer></footer>
     </>;
 }
@@ -31,6 +27,13 @@ const StyledHeader = styled.header`
     font-size: 3.5rem;
   }
 `
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--color-white);
+`
 
 const StyledImg = styled.img`
   width: 80%;
@@ -38,19 +41,6 @@ const StyledImg = styled.img`
   justify-content: center;
 `
 
-const StyledDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  background-color: white;
-`
-
-const StyledDiv2 = styled.div`
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  display: flex;
-  justify-content: center;
-  background-color: white;
-`
 const StyledLink = styled(Link)`
   padding-top: 1rem;
   padding-bottom: 1rem;
@@ -67,4 +57,6 @@ const StyledLink = styled(Link)`
 const StyledButton = styled.button`
   border: none;
   background-color: transparent;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
 `
