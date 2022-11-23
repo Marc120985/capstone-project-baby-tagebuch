5b05c0e253443510cbfcde81a2d3a9ba6dad2e23
Marc120985 <creativ.zeit@gmail.com>
2022-11-23 14:49:03 +0100

    styled BabyPage.tsx

--- frontend/src/BabyPage.tsx
+++ frontend/src/BabyPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BabyModel} from "./BabyModel";
 import {useParams} from "react-router";
 import styled from "styled-components";
+import {Link} from "react-router-dom";
 
 type babyProps = { babies: BabyModel[] }
 
@@ -27,19 +28,29 @@ export default function BabyPage(props: babyProps) {
         </StyledHeader>
 
         <StyledSection>
-            <div>
-            <p>{foundBaby.birthday}</p>
-            </div>
-            <div>
-            <p>{foundBaby.weight}</p>
-            </div>
-            <p>{foundBaby.height}</p>
-            <p>{foundBaby.gender}</p>
-            <button>Update Baby</button>
-            <button>Delete Baby</button>
-            <button>Essen</button>
-            <button>Schlafen</button>
+            <StyledH4>Geburtstag</StyledH4>
+            <StyledP>{foundBaby.birthday}</StyledP>
+            <StyledH4>Gewicht in Gramm</StyledH4>
+            <StyledP>{foundBaby.weight}</StyledP>
+            <StyledH4>Größe in Zentimetern</StyledH4>
+            <StyledP>{foundBaby.height}</StyledP>
+            <StyledH4>Geschlecht</StyledH4>
+            <StyledP>{foundBaby.gender}</StyledP>
         </StyledSection>
+        <StyledSection2>
+        <StyledButton>Essen</StyledButton>
+        <StyledButton>Schlafen</StyledButton>
+        </StyledSection2>
+        <StyledSection2>
+            <StyledButton3>Update Baby</StyledButton3>
+            <StyledButton3>Delete Baby</StyledButton3>
+        </StyledSection2>
+
+        <div>
+            <StyledButton2>
+                <StyledLink2 to={"/Babyoverview"}>Alle Baby's</StyledLink2>
+            </StyledButton2>
+        </div>
     </>;
 }
 
@@ -60,3 +71,74 @@ const StyledSection = styled.section`
   background-color: white;
   align-items: center;
 `
+const StyledLink2 = styled(Link)`
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 5rem;
+  padding-right: 5rem;
+  background-color: var(--color-background);
+  display: flex;
+  justify-content: center;
+  border-radius: 1rem;
+  color: var(--color-white);
+  font-family: Ubuntu, sans-serif;
+  text-decoration: none;
+  margin: 0.5rem;
+  border: 1px solid white;
+`
+const StyledButton2 = styled.button`
+  border: none;
+  background-color: transparent;
+  width: 100%;
+`
+
+const StyledH4 = styled.h4`
+  font-family: Gistesy, sans-serif;
+  font-size: 2.6rem;
+  color: var(--color-background);
+  text-shadow: 1px 1px 1px black;
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+`
+
+const StyledP = styled.p`
+  font-family: ubuntu, sans-serif;
+  font-size: 1.5rem;
+  color: var(--color-black);
+  text-shadow: 1px 1px 1px var(--color-background);
+  padding: 5px 20px 5px 20px;
+  border: 1px solid var(--color-background);
+  border-radius: 1rem;
+`
+const StyledSection2 = styled.section`
+  display: flex;
+  justify-content: center;
+  background-color: white;
+`
+
+const StyledButton = styled.button`
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 5rem;
+  padding-right: 5rem;
+  background-color: var(--color-background);
+  border: none;
+  border-radius: 1rem;
+  margin: 0.5rem;
+  color: var(--color-white);
+  font-family: ubuntu, sans-serif;
+`
+const StyledButton3 = styled.button`
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    padding-left: 5rem;
+    padding-right: 5rem;
+    background-color: transparent;
+    border: 1px solid var(--color-background);
+    border-radius: 1rem;
+    margin: 0.5rem;
+    color: var(--color-background);
+    font-family: ubuntu, sans-serif;
+`
+
+
