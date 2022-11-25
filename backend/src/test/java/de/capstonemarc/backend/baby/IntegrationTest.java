5b920ec8036aa225ca54aa7930db2da957afb44a
Marc120985 <creativ.zeit@gmail.com>
2022-11-25 12:03:58 +0100

    add put method and junit test for method

--- backend/src/test/java/de/capstonemarc/backend/baby/IntegrationTest.java
+++ backend/src/test/java/de/capstonemarc/backend/baby/IntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -68,5 +69,68 @@ class IntegrationTest {
                                 "gender": "w"
                                 }
                                 """.replace("<id>", baby.id())));
+    }
+
+    @DirtiesContext
+    @Test
+    void updatebabyWithBeforePostBabyAndChangedWeight() throws Exception {
+        //given
+        String content = mockMvc.perform(MockMvcRequestBuilders.post("/api/babies")
+
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "name": "Rabbit",
+                                    "birthday": "01.01.2000",
+                                    "weight": "3500",
+                                    "height": "76",
+                                    "gender": "w"
+                                }
+                                """))
+                .andExpect(status().is(201))
+                .andReturn().getResponse().getContentAsString();
+        Baby baby = objectMapper.readValue(content, Baby.class);
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/babies/"))
+                .andExpect(status().is(200))
+                .andExpect(content().json("""
+                        [{
+                            "id": "<id>",
+                            "name": "Rabbit",
+                            "birthday": "01.01.2000",
+                            "weight": "3500",
+                            "height": "76",
+                            "gender": "w"
+                        }]
+                        """.replace("<id>", baby.id())));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/babies/" + baby.id())
+
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "id": "<id>",
+                                    "name": "Rabbit",
+                                    "birthday": "01.01.2000",
+                                    "weight": "8000",
+                                    "height": "76",
+                                    "gender": "w"
+                                }
+                                """.replace("<id>", baby.id())))
+                .andExpect(status().is(200))
+                .andReturn().getResponse().getContentAsString();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/babies/"))
+                .andExpect(status().is(200))
+                .andExpect(content().json("""
+                        [{
+                            "id": "<id>",
+                            "name": "Rabbit",
+                            "birthday": "01.01.2000",
+                            "weight": "8000",
+                            "height": "76",
+                            "gender": "w"
+                        }]
+                        """.replace("<id>", baby.id())));
     }
 }
